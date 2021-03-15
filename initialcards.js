7308409b01f2ad7da077b1bad28f7f4392f67332
Chris <65090952+chriszabo@users.noreply.github.com>
2021-03-15 23:42:10 +0100

    fixed

--- initialcards.js
+++ initialcards.js
@@ -1,17 +1,16 @@
-const panels = document.querySelectorAll('panel')
+const panels = document.querySelectorAll('.panel');
+//console.log(panels[1]);
 
-/*console.log(panels[1])*/
-
-panels.forEach(panel)  ; {
+panels.forEach(panel => {
     panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
+        removeActiveClasses();
+        panel.classList.add('active');
     })
-}
+});
 
 function removeActiveClasses() {
-    panel.forEach(panel => {
-        panel.classList.remove('active')
+    panels.forEach(panel => {
+        panel.classList.remove('active');
     })
 }
 
